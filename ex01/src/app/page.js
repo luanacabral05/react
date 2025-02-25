f2a46121ccadedb09d244a2d2dd0ed63e8aa6286
Luana Cabral <163360766+luanacabral05@users.noreply.github.com>
2025-02-25 00:10:42 +0000

    Usando o hook useState

--- ex01/src/app/page.js
+++ ex01/src/app/page.js
@@ -1,15 +1,27 @@
+"use client";
+
+import { useState } from "react";
+
 function Texto1() {
   return <p>Um parágrafo de texto</p>;
 }
 
 export default function Home() {
+  const [hide, setHide] = useState(false);
   const aula = 2;
   return (
     <div>
     <h1>Hello Lua!</h1>
       <p> {aula}ª aula  de React </p>
-      <Texto1 />
-      <Texto1 />
+      <hr />
+      <button onClick={() => setHide(!hide)}>{hide ? "Abrir" : "Fechar"}</button>
+      <hr />
+      {!hide && (
+        <>
+          <Texto1 />
+          <Texto1 />
+        </>
+      )}
     </div>
-  );
+  )
 }
